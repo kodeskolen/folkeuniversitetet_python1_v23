a7e8a19df74e34cdd70b7e6ff528f67a7215da0f
Johan Emil <jelarsso@uio.no>
2023-01-31 14:04:53 +0100

    La til materiale

---
+++ dag2/eksempler/velkommen_tilbake.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+print("Velkommen tilbake!")
+print("Programmering med Python - Dag 2")
+
+# Bruker datetime pakken til å skrive dagens dato.
+from datetime import date
+print(date.today())
+
+print()
+
+print("Tema for i dag er")
+print("- Repetisjon")
+print("- Lister")
+print("- Mer om løkker")
+print("- Tekstbehandling")
+
